3634e564cdb84443fe5d66bb513b6bd564b8af9a
chinesedfan <chinesedfan@qq.com>
2016-04-10 13:41:31 +0800

    use the strategy `most+rest`

--- 2016/d.js
+++ 2016/d.js
@@ -11,26 +11,56 @@ _.each(lines, function(line, i) {
         t = parseInt(line);
         return;
     } else if (i <= t) {
-        console.log('Case #%d: %s', i, solve(line));
+        var tokens = line.split(' ');
+        var k = parseInt(tokens[0]);
+        var c = parseInt(tokens[1]);
+        var s = parseInt(tokens[2]);
+        console.log('Case #%d: %s', i, solve(k, c, s));
     }
 });
 
-function solve(line) {
-    var tokens = line.split(' ');
-    var k = parseInt(tokens[0]);
-    var c = parseInt(tokens[1]);
-    var s = parseInt(tokens[2]);
+/**
+ * Suppose L = 1, G = 0, the origin artwork is [T0, T1, ..., Tk-1]
+ * If c = 1, and present the position as `a`(base k), then the tile is `Ta`
+ * If c = 2, and present the position as `ab`(base k), then the tile is `Ta & Tb`
+ * So it requires to check `k + 1 - c` tiles at least
+ */
+function solve(k, c, s) {
+    if (s < k + 1 - c) {
+        return 'IMPOSSIBLE';
+    }
 
-    return fSmall(k, c);
+    var result = [];
+    result.push(posForMost(k, c));
+    for (var i = c; i < k; i++) {
+        result.push(posForSingle(k, c, i));
+    }
+    return result.join(' ');
 }
-function fSmall(k, c) {
+/**
+ * @return {String} the position which only affected by tile i
+ */
+function posForSingle(k, c, i) {
     if (k == 1) return 1; // starts from 1
 
     var bigK = BigInteger(k);
     var bigX = bigK.pow(c).subtract(1).divide(k - 1);
-    var pos = [];
-    for (var i = 0; i < k; i++) {
-        pos.push(bigX.multiply(i).add(1).toString()); // starts from 1
+    // the sum of i*k^0 + i*k^1 + ... + i*k^(c-1) = i*(k^c - 1)/(k - 1)
+    return bigX.multiply(i).add(1).toString();
+}
+/**
+ * @return {String} the position which contains tiles 0 to c - 1
+ */
+function posForMost(k, c) {
+    // the sum of 0*k^0 + 1*k^1 + ... + (c-1)*k^(c-1)
+    var i = 0, x = BigInteger(1),
+        limit = Math.min(k, c),
+        sum = BigInteger(0);
+
+    while (i < limit) {
+        sum = sum.add(x.multiply(i));
+        i++;
+        x = x.multiply(k);
     }
-    return pos.join(' ');
+    return sum.add(1).toString();
 }
