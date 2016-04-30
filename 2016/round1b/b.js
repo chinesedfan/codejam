9090cb1d215d7ebc017776e2d7153f80048fe9ad
chinesedfan <chinesedfan@qq.com>
2016-05-01 02:02:20 +0800

    fix cases like `?9 ?0`

--- 2016/round1b/b.js
+++ 2016/round1b/b.js
@@ -12,7 +12,7 @@ for (var i = 0; i < t; i++) {
 }
 
 function solve(s1, s2) {
-    var diffPos = -1;
+    var diffPos = -1, reverse;
     _.some(s1, function(c, i) {
         if (s1[i] != s2[i] && s1[i] != '?' && s2[i] != '?') {
             diffPos = i;
@@ -21,7 +21,12 @@ function solve(s1, s2) {
     });
 
     var results = _.map([-1, 0, 1], function(compare) {
-        var arr = tryWithCompare(s1, s2, diffPos, compare);
+        reverse = false;
+        if (diffPos >= 0 && compareChar(s1[diffPos], s2[diffPos]) != compare) {
+            reverse = true;
+        }
+
+        var arr = tryWithCompare(s1, s2, diffPos, compare, reverse);
         //console.log(arr.toString())
         var item = {
             cstr: arr[0],
@@ -52,16 +57,41 @@ function solve(s1, s2) {
     return min.cstr + ' ' + min.jstr;
 }
 
-function tryWithCompare(s1, s2, diffPos, compare) {
-    var x1 = [], x2= [];
+function tryWithCompare(s1, s2, diffPos, compare, reverse) {
+    var x1 = [], x2 = [], v1, v2;
+    var onePos = -1;
+    if (reverse) {
+        _.some(s1, function(c, i) {
+            if (s1[i] == '?' && s2[i] != '?') {
+                if (compare < 0 && s2[i] != '0') { onePos = i; return true; }
+                if (compare > 0 && s2[i] != '9') { onePos = i; return true; }
+            }
+            if (s1[i] != '?' && s2[i] == '?') {
+                if (compare < 0 && s1[i] != '9') { onePos = i; return true; }
+                if (compare > 0 && s1[i] != '0') { onePos = i; return true; }
+            }
+            if (s1[i] == s2[i] && s1[i] == '?' && onePos < 0) {
+                onePos = i;
+            }
+        });
+    }
     _.each(s1, function(c, i) {
         if (s1[i] != '?' && s2[i] != '?') {
             x1.push(s1[i]);
             x2.push(s2[i]);
         } else if (s1[i] == '?' && s2[i] == '?') {
             if (i < diffPos) {
-                x1.push('0');
-                x2.push('0');
+                v1 = '0';
+                v2 = '0';
+                if (i == onePos) {
+                    if (compare < 0) v2 = '1';
+                    if (compare > 0) v1 = '1';
+                } else if (onePos >= 0 && i > onePos) {
+                    if (compare < 0) v1 = '9';
+                    if (compare > 0) v2 = '9';
+                }
+                x1.push(v1);
+                x2.push(v2);
             } else if (compare < 0) {
                 x1.push('9');
                 x2.push('0');
@@ -74,7 +104,15 @@ function tryWithCompare(s1, s2, diffPos, compare) {
             }
         } else if (s1[i] == '?') {
             if (i < diffPos) {
-                x1.push(s2[i]);
+                v1 = s2[i];
+                if (i == onePos) {
+                    if (compare < 0) v1 = (parseInt(s2[i]) - 1);
+                    if (compare > 0) v1 = (parseInt(s2[i]) + 1);
+                } else if (onePos >= 0 && i > onePos) {
+                    if (compare < 0) v1 = '9';
+                    if (compare > 0) v1 = '0';
+                }
+                x1.push(v1);
             } else if (compare < 0) {
                 x1.push('9');
             } else if (compare > 0) {
@@ -85,7 +123,15 @@ function tryWithCompare(s1, s2, diffPos, compare) {
             x2.push(s2[i]);
         } else {
             if (i < diffPos) {
-                x2.push(s1[i]);
+                v2 = s1[i];
+                if (i == onePos) {
+                    if (compare < 0) v2 = (parseInt(s1[i]) + 1);
+                    if (compare > 0) v2 = (parseInt(s1[i]) - 1);
+                } else if (onePos >= 0 && i > onePos) {
+                    if (compare < 0) v2 = '0';
+                    if (compare > 0) v2 = '9';
+                }
+                x2.push(v2);
             } else if (compare < 0) {
                 x2.push('0');
             } else if (compare > 0) {
@@ -96,6 +142,7 @@ function tryWithCompare(s1, s2, diffPos, compare) {
             x1.push(s1[i]);
         }
     });
+    if (x1.length != x2.length) throw new Error('different length');
     return [x1.join(''), x2.join('')];
 }
 
