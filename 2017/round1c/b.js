754f5dc6a2ab88b5c091c07c6db72164f97548cb
chinesedfan <chinesedfan@qq.com>
2017-04-30 22:02:33 +0800

    17.1c.b, try large

--- 2017/round1c/b.js
+++ 2017/round1c/b.js
@@ -15,6 +15,7 @@ while (i <= t && l < lines.length) {
     while (arr1.length < a) {
         token = lines[l++].split(' ');
         arr1.push({
+            type: 'a',
             start: parseInt(token[0]),
             end: parseInt(token[1])
         });
@@ -23,6 +24,7 @@ while (i <= t && l < lines.length) {
     while (arr2.length < b) {
         token = lines[l++].split(' ');
         arr2.push({
+            type: 'b',
             start: parseInt(token[0]),
             end: parseInt(token[1])
         });
@@ -31,15 +33,54 @@ while (i <= t && l < lines.length) {
 }
 
 function solve(arr1, arr2) {
-    if (arr1.length > arr2.length) return solve(arr2, arr1);
+    if (arr1.length + arr2.length <= 1) return 2;
 
-    arr1 = _.sortBy(arr1, (item) => item.start);
-    arr2 = _.sortBy(arr2, (item) => item.start);
-    if (arr1.length == 0) {
-        if (arr2.length < 2) return 2;
+    var total1 = 0, total2 = 0;
+    var gaps = [], gap1 = [], gap2 = [];
+    var sorted = _(arr1).concat(arr2).sortBy(byStart).value();
+    _.each(sorted, (item, i) => {
+        var prev = sorted[i ? i - 1 : sorted.length - 1];
+        var delta = item.start - prev.end;
+        if (delta < 0) delta += 1440;
 
-        return (arr2[1].end - arr2[0].start > 720 && arr2[0].end + 1440 - arr2[1].start > 720) ? 4 : 2;
-    } else { // arr1.length == 1
-        return 2;
+        var duration = item.end - item.start;
+        if (prev.type == item.type) {
+            duration += delta;
+        } else {
+            gaps.push(delta);
+        }
+
+        if (item.type == 'a') {
+            total1 += duration;
+            gap1.push(delta);
+        } else {
+            total2 += duration;
+            gap2.push(delta);
+        }
+    });
+
+    var common = _.sum(gaps);
+    var exchange = gaps.length;
+    if (total1 < total2) {
+        total1 += common;
+        exchange += 2 * moveGaps(total1, gap2);
+    } else {
+        total2 += common;
+        exchange += 2 * moveGaps(total2, gap1);
     }
+    return exchange;
+}
+function moveGaps(total, gaps) {
+    gaps = _.sortBy(gaps);
+
+    var move = 0;
+    while (gaps.length && total < 720) {
+        total += gaps.pop();
+        move++;
+    }
+    return move;
+}
+
+function byStart(item) {
+    return item.start;
 }
