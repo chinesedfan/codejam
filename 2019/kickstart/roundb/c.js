4386e15ef370bd89a93d6060c4981cac6c888f4e
Xianming Zhong <chinesedfan@qq.com>
2019-04-21 09:16:16 +0800

    n^2, wa

--- 2019/kickstart/roundb/c.js
+++ 2019/kickstart/roundb/c.js
@@ -20,24 +20,19 @@ rl.on('close', function() {
 });
 
 function solve(n, s, ts) {
-    var cs = [];
-    var count = {}; // ch -> count
-    for (var i = 0; i < ts.length; i++) {
-        count[ts[i]] = (count[ts[i]] || 0) + 1;
-        cs.push(Object.assign({}, count));
-    }
-
     var mx = -Infinity;
     for (var i = 0; i < ts.length; i++) {
+        var count = {}; // ch -> count
+        var sum = 0;
         for (var j = i; j < ts.length; j++) {
-            var beg = cs[i - 1];
-            var end = cs[j];
-            var sum = 0;
-            for (var key in end) {
-                var before = beg ? (+beg[key] || 0) : 0;
-                var after = +end[key];
-                sum += (after - before > s) ? 0 : after - before;
+            var type = ts[j];
+            count[type] = (count[type] || 0) + 1;
+
+            sum++;
+            if (count[type] > s) {
+                sum -= count[type];
             }
+
             mx = Math.max(mx, sum);
         }
     }
