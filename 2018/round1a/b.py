51f2138a0e43dc36af1f4895a46be48b6c5f32c4
Xianming Zhong <chinesedfan@qq.com>
2019-04-27 17:53:28 +0800

    fix float, still tle

--- 2018/round1a/b.py
+++ 2018/round1a/b.py
@@ -1,21 +1,28 @@
 import math
 
 def getCap(t, item):
-    [m, s, p] = item
+    [m, s, p, _] = item
     return max(0, min(m, math.floor((float(t) - p) / s)))
 
 def check(t, cs, r, b):
-    cs = sorted(cs, lambda c1, c2: int(getCap(t, c2) - getCap(t, c1)))
-    return b <= sum(map(lambda item: getCap(t, item), cs[0:r]))
+    for item in cs:
+        item[3] = getCap(t, item)
+    cs = sorted(cs, lambda c1, c2: int(c2[3] - c1[3]))
+    return b <= sum(map(lambda item: item[3], cs[0:r]))
 
 def solve(r, b, c, cs):
-    ms = max(cs, key=lambda item: item[1])[1]
-    mp = max(cs, key=lambda item: item[2])[2]
+    ms = cs[0][1]
+    mp = cs[0][2]
+    for item in cs:
+        if item[1] > ms:
+            ms = item[1]
+        if item[2] > mp:
+            mp = item[2]
 
     tmin = 0
     tmax = b * ms + mp
     while tmin < tmax:
-        middle = long(math.floor((tmin + tmax) / 2))
+        middle = (tmin + tmax) / 2  # keep as int, instead of float
         if check(middle, cs, r, b):
             tmax = middle
         else:
@@ -31,6 +38,6 @@ for i in xrange(1, t + 1):
     cs = []
     for j in xrange(c):
         m, s, p = map(int, raw_input().split(' '))
-        cs.append([m, s, p])
+        cs.append([m, s, p, 0])
 
     print 'Case #{}: {}'.format(i, solve(r, b, c, cs))
