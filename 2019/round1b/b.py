9b7cd0e0c132bcfff7c36814d5dff55a5d4f0d5d
Xianming Zhong <chinesedfan@qq.com>
2019-04-30 19:27:03 +0800

    wa

--- 2019/round1b/b.py
+++ 2019/round1b/b.py
@@ -7,19 +7,19 @@ while c < t:
     c = c + 1
 
     s = []
-    for x in [1,2,3,4,5,6]:
+    for x in [56, 200]: # as well as their gaps are larger than 7, because 2^7 > 100
         print x
         sys.stdout.flush()
 
         s.append(int(raw_input()))
 
     r = [0] * 6
-    r[0] = (s[5] - 2 * s[2] - 4 * (s[1] - s[0])) / 40
-    r[1] = s[1] - s[0] - 2 * r[0]
-    r[2] = s[2] - s[1] - 4 * r[0]
-    r[3] = s[3] - s[2] - (8 * r[0] + 2 * r[1])
-    r[4] = s[4] - s[3] - 16 * r[0]
-    r[5] = s[5] - s[4] - (32 * r[0] + 4 * r[1] + 2 * r[2])
+    r[0] = s[0] / (2 ** 56)
+    r[1] = s[0] / (2 ** 28) - r[0] * 2 ** 28
+    r[2] = s[0] / (2 ** 18) - r[0] * 2 ** 38 - r[1] * 2 ** 10
+    r[3] = s[1] / (2 ** 50)
+    r[4] = s[1] / (2 ** 40) - r[3] * 2 ** 10
+    r[5] = s[1] / (2 ** 33) - r[3] * 2 ** 17 - r[4] * 2 ** 7
 
     print ' '.join(map(str, r))
     sys.stdout.flush()
