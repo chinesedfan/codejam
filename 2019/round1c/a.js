2f82a23912985dee5a4051bbcaf1dc605eebc438
Xianming Zhong <chinesedfan@qq.com>
2019-05-05 10:20:35 +0800

    fix ties by backtracing, wa

--- 2019/round1c/a.js
+++ 2019/round1c/a.js
@@ -21,31 +21,69 @@ rl.on('close', function() {
 function solve(n, ps) {
     var mlen = Math.max.apply(Math, ps.map((p) => p.length));
     var ret = [];
+    var beats = {};
+    var stack = [];
 
-    for (var i = 0; i < mlen; i++) {
-        var cans = {R: 1, P: 1, S: 1};
+    var i = 0;
+    while (i <= mlen) {
+        var chs = {R: [], P: [], S: []};
         for (var j = 0; j < ps.length; j++) {
+            if (beats[j]) continue;
+
             var ri = i % ps[j].length;
             var other = ps[j][ri];
-            if (other === 'S') {
-                cans.P = 0;
-            } else if (other === 'P') {
-                cans.R = 0;
-            } else if (other === 'R') {
-                cans.S = 0;
-            }
+            chs[other].push(j);
         }
 
-        if (cans.R) {
-            ret.push('R');
-        } else if (cans.S) {
-            ret.push('S');
-        } else if (cans.P) {
-            ret.push('P');
+        var next = [];
+        if (!chs.P.length) next.push('R');
+        if (!chs.R.length) next.push('S');
+        if (!chs.S.length) next.push('P');
+
+        var all = true;
+        if (i == mlen) {
+            for (var j = 0; j < ps.length; j++) {
+                if (!beats[j]) {
+                    all = false;
+                    break;
+                }
+            }
+            if (all) break;
+        }
+
+        if (next.length == 0 || !all) {
+            if (!stack.length) return 'IMPOSSIBLE';
+
+            var state = stack.pop();
+            ret = state.ret;
+            beats = state.beats;
+            i = state.i;
+
+            chs[getCanBeat(state.other)].forEach((x) => beats[x] = 1);
+            ret.push(state.other);
+            i++;
         } else {
-            return 'IMPOSSIBLE';
+            if (next.length == 2) {
+                stack.push({
+                    ret: ret.slice(0),
+                    beats: Object.assign(beats),
+                    i: i,
+                    other: next[1]
+                });
+            } // ignore if equals 3
+
+            chs[getCanBeat(next[0])].forEach((x) => beats[x] = 1);
+            ret.push(next[0]);
+            i++;
         }
     }
 
     return ret.join('');
+}
+
+function getCanBeat(ch) {
+    if (ch === 'R') return 'S';
+    if (ch === 'S') return 'P';
+    if (ch === 'P') return 'R';
+    throw new Error('unknown ch:', ch);
 }
