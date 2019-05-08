a726ccebaa64ce14d35abfac88d4b8559b9ff31c
Xianming Zhong <chinesedfan@qq.com>
2019-05-08 23:22:19 +0800

    small, not large

--- 2013/kickstart/roundb/c.js
+++ 2013/kickstart/roundb/c.js
@@ -26,75 +26,72 @@ function solve(grid) {
     var rs = [];
     for (var i = 0; i < grid.length; i++) {
         if (grid[0][i] == 'R') {
-            var n = {x: 0, y: i};
-            rs.push(n);
+            rs.push({x: 0, y: i, l: 0});
         }
     }
     var r1 = bfs(grid, rs, {}, 'R', (x) => x.x == grid.length - 1);
-    var r2 = bfs(grid, r1, {}, 'R', (x) => x.x == 0, true, 0);
+    var r2 = bfs(grid, r1, {}, 'R', (x) => x.x == 0, true);
     if (r2.length) {
-        if (rn >= bn) return 'Red wins';
+        if (r2.valid && rn >= bn) return 'Red wins';
         else return 'Impossible';
     }
 
     var bs = [];
     for (var i = 0; i < grid.length; i++) {
         if (grid[i][0] == 'B') {
-            var n = {x: i, y: 0};
-            bs.push(n);
+            bs.push({x: i, y: 0, l: 0});
         }
     }
     var b1 = bfs(grid, bs, {}, 'B', (x) => x.y == grid.length - 1);
-    var b2 = bfs(grid, b1, {}, 'B', (x) => x.y == 0, true, 1);
+    var b2 = bfs(grid, b1, {}, 'B', (x) => x.y == 0, true);
     if (b2.length) {
-        if (bn >= rn) return 'Blue wins';
+        if (b2.valid && bn >= rn) return 'Blue wins';
         else return 'Impossible';
     }
 
     return 'Nobody wins';
 }
 
-function bfs(grid, q, visited, color, fn, twice, groupbyindex) {
+function bfs(grid, q, visited, color, fn, twice) {
     q = q.slice(0);
 
     var c = [];
+    var m = {}; // l -> c
+    var l = Infinity;
     while (q.length) {
         var n = q.shift();
         if (visited[key(n)]) continue;
         visited[key(n)] = 1;
-        if (fn(n)) c.push(n);
+        m[n.l] = (m[n.l] || 0) + 1;
+        if (fn(n)) {
+            c.push({x: n.x, y: n.y, l: 0}); // clean l for reuse
+            l = Math.min(l, n.l);
+        }
 
-        add(q, visited, grid, n.x, n.y - 1, color);
-        add(q, visited, grid, n.x, n.y + 1, color);
-        add(q, visited, grid, n.x - 1, n.y, color);
-        add(q, visited, grid, n.x - 1, n.y + 1, color);
-        add(q, visited, grid, n.x + 1, n.y, color);
-        add(q, visited, grid, n.x + 1, n.y - 1, color);
+        add(q, visited, grid, n.x, n.y - 1, n.l + 1, color);
+        add(q, visited, grid, n.x, n.y + 1, n.l + 1, color);
+        add(q, visited, grid, n.x - 1, n.y, n.l + 1, color);
+        add(q, visited, grid, n.x - 1, n.y + 1, n.l + 1, color);
+        add(q, visited, grid, n.x + 1, n.y, n.l + 1, color);
+        add(q, visited, grid, n.x + 1, n.y - 1, n.l + 1, color);
     }
 
     if (twice) {
-        var m = {};
-        for (var str in visited) {
-            var fs = str.split('#');
-            var k = fs[groupbyindex];
-            var v = fs[groupbyindex ? 0 : 1];
-            m[k] = m[k] || [];
-            m[k].push(v);
-        }
         var flag = false;
         for (var k in m) {
-            if (m[k].length == 1) {
+            if (+k > l) continue; // ingore if connected
+            if (m[k] == 1) {
                 flag = true;
                 break;
             }
         }
-        if (!flag) c = [];
+        c.valid = flag;
     }
 
     return c;
 }
-function add(q, visited, grid, x, y, color) {
-    var n = {x: x, y: y};
+function add(q, visited, grid, x, y, l, color) {
+    var n = {x: x, y: y, l: l};
     if (x >= 0 && x < grid.length
         && y >= 0 && y < grid.length
         && !visited[key(n)]
