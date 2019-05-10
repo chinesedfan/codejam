331b29b2c7bc28f3401cb0b836a4fbce03835552
Xianming Zhong <chinesedfan@qq.com>
2019-05-10 22:26:05 +0800

    pass c with the simple way

--- 2013/kickstart/roundb/c.js
+++ 2013/kickstart/roundb/c.js
@@ -8,90 +8,78 @@ var t = parseInt(lines[0]);
 var l = 1;
 for (var i = 0; i < t; i++) {
     var n = +lines[l++];
-    console.log('Case #%d: %s', i + 1, solve(lines.slice(l, l + n)));
+    console.log('Case #%d: %s', i + 1, solve(lines.slice(l, l + n).map((str) => str.split(''))));
     l += n;
 }
 
 function solve(grid) {
-    var rn = 0;
-    var bn = 0;
+    var rs = [];
+    var bs = [];
     for (var i = 0; i < grid.length; i++) {
         for (var j = 0; j < grid.length; j++) {
-            if (grid[i][j] == 'R') rn++;
-            if (grid[i][j] == 'B') bn++;
+            if (grid[i][j] == 'R') rs.push({x: i, y: j});
+            if (grid[i][j] == 'B') bs.push({x: i, y: j});
         }
     }
-    if (Math.abs(rn - bn) > 1) return 'Impossible';
+    if (Math.abs(rs.length - bs.length) > 1) return 'Impossible';
 
-    var rs = [];
-    for (var i = 0; i < grid.length; i++) {
-        if (grid[0][i] == 'R') {
-            rs.push({x: 0, y: i, l: 0});
-        }
-    }
-    var r1 = bfs(grid, rs, {}, 'R', (x) => x.x == grid.length - 1);
-    var r2 = bfs(grid, r1, {}, 'R', (x) => x.x == 0, true);
-    if (r2.length) {
-        if (r2.valid && rn >= bn) return 'Red wins';
+    var wr;
+    wr = win(grid, rs, rs.filter((n) => n.x == 0), 'R', (n) => n.x == grid.length - 1);
+    if (wr.connected) {
+        if (wr.valid && rs.length >= bs.length) return 'Red wins';
         else return 'Impossible';
     }
 
-    var bs = [];
-    for (var i = 0; i < grid.length; i++) {
-        if (grid[i][0] == 'B') {
-            bs.push({x: i, y: 0, l: 0});
-        }
-    }
-    var b1 = bfs(grid, bs, {}, 'B', (x) => x.y == grid.length - 1);
-    var b2 = bfs(grid, b1, {}, 'B', (x) => x.y == 0, true);
-    if (b2.length) {
-        if (b2.valid && bn >= rn) return 'Blue wins';
+    wr = win(grid, bs, bs.filter((n) => n.y == 0), 'B', (n) => n.y == grid.length - 1);
+    if (wr.connected) {
+        if (wr.valid && bs.length >= rs.length) return 'Blue wins';
         else return 'Impossible';
     }
 
     return 'Nobody wins';
 }
 
-function bfs(grid, q, visited, color, fn, twice) {
+function win(grid, ss, rs, color, fn) {
+    var connected = bfs(grid, rs, {}, color, fn);
+    if (!connected) return {connected: false};
+
+    var valid = false;
+    for (var i = 0; i < ss.length; i++) {
+        var node = ss[i];
+        grid[node.x][node.y] = '.';
+        connected = bfs(grid, rs, {}, color, fn);
+        if (!connected) valid = true;
+        grid[node.x][node.y] = color;
+        if (valid) break;
+    }
+    return {
+        connected: true,
+        valid: valid
+    };
+}
+
+function bfs(grid, q, visited, color, fn) {
     q = q.slice(0);
 
-    var c = [];
-    var m = {}; // l -> c
-    var l = Infinity;
     while (q.length) {
         var n = q.shift();
+        if (grid[n.x][n.y] === '.') continue;
         if (visited[key(n)]) continue;
         visited[key(n)] = 1;
-        m[n.l] = (m[n.l] || 0) + 1;
-        if (fn(n)) {
-            c.push({x: n.x, y: n.y, l: 0}); // clean l for reuse
-            l = Math.min(l, n.l);
-        }
+        if (fn(n)) return true;
 
-        add(q, visited, grid, n.x, n.y - 1, n.l + 1, color);
-        add(q, visited, grid, n.x, n.y + 1, n.l + 1, color);
-        add(q, visited, grid, n.x - 1, n.y, n.l + 1, color);
-        add(q, visited, grid, n.x - 1, n.y + 1, n.l + 1, color);
-        add(q, visited, grid, n.x + 1, n.y, n.l + 1, color);
-        add(q, visited, grid, n.x + 1, n.y - 1, n.l + 1, color);
+        add(q, visited, grid, n.x, n.y - 1, color);
+        add(q, visited, grid, n.x, n.y + 1, color);
+        add(q, visited, grid, n.x - 1, n.y, color);
+        add(q, visited, grid, n.x - 1, n.y + 1, color);
+        add(q, visited, grid, n.x + 1, n.y, color);
+        add(q, visited, grid, n.x + 1, n.y - 1, color);
     }
 
-    if (twice) {
-        var flag = false;
-        for (var k in m) {
-            if (+k > l) continue; // ingore if connected
-            if (m[k] == 1) {
-                flag = true;
-                break;
-            }
-        }
-        c.valid = flag;
-    }
-
-    return c;
+    return false;
 }
-function add(q, visited, grid, x, y, l, color) {
-    var n = {x: x, y: y, l: l};
+function add(q, visited, grid, x, y, color) {
+    var n = {x: x, y: y};
     if (x >= 0 && x < grid.length
         && y >= 0 && y < grid.length
         && !visited[key(n)]
