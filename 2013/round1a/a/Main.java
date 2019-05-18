1bc6403e789f76ea1f75ea321f3e4e23ca2edb7f
Xianming Zhong <chinesedfan@qq.com>
2019-05-18 16:09:32 +0800

    a large

--- 2013/round1a/a/Main.java
+++ 2013/round1a/a/Main.java
@@ -7,19 +7,33 @@ public class Main {
 		int c = 1;
 		while (c <= t) {
 			long r = s.nextLong();	
-			r++;
 			long total = s.nextLong();	
-			int count = 0;
-			while (total > 0) {
-				long ri = r - 1;
-				long temp = (r*r-ri*ri);
-//System.out.println(total + " " + temp);
-				if (total < temp) break;
-				total -= temp;
-				r += 2;
-				count++;
+
+			long min = 1;
+			long max = 1;
+			while (f(r, max) <= total) max = max << 1;
+
+			// binary search			
+			while (max >= min) {
+				long middle = min + (max - min) / 2;
+				if (f(r, middle) <= total) {
+					min = middle + 1;	
+				} else {
+					max = middle - 1;	
+				}
 			}
-			System.out.println("Case #" + (c++) + ": " + count);
+
+			System.out.println("Case #" + (c++) + ": " + max);
 		}
+	}
+
+	public static long f(long r, long n) {
+		// for each ring that has r as white radius,
+		// the cost is (r + 1)^2 - r^2 = 2r + 1
+		// so, suppose the last white disk is r + 2 * (n - 1), then the sum is,
+		// sum(2 * (r + 2 * (i - 1)) + 1), where i = [1, n]
+		// = 2 * (n * r + n * (n - 1)) + n
+		// = 2 * n^2 + 2 * n * r - n
+		return 2 * n * n + 2 * n * r - n;
 	}
 }
