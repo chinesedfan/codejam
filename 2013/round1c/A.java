41e5eb01ef43a796217dac91f98b679e0de88058
Xianming Zhong <chinesedfan@qq.com>
2019-05-25 17:44:01 +0800

    fail on large numbers

--- 2013/round1c/A.java
+++ 2013/round1c/A.java
@@ -1,6 +1,4 @@
-package codejam1c;
-
-import java.util.Scanner;
+import java.util.*;
 
 public class A {
 	public static void main(String args[]) {
@@ -10,8 +8,11 @@ public class A {
 			String name = s.next();
 			int n = s.nextInt();
 			
+			Vector<Integer> vec = new Vector<Integer>();
 			int[] count = new int[name.length()];
+			int[] end = new int[name.length()];
 			for (int i = 0; i < name.length(); i++) {
+				end[i] = -1;
 				if (notVowel(name.charAt(i))) {
 					if (i == 0) {
 						count[i] = 1;
@@ -22,19 +23,30 @@ public class A {
 					count[i] = 0;	
 				}
 				//System.out.println(count[i]);
+
+				vec.add(i);
+
+				int ci = count[i];
+				if (ci >= n) {
+					while (vec.size() > 0) {
+						int top = vec.elementAt(0);
+
+						int d = i - top + 1;
+						int rc = d < ci ? d : ci;
+						if (rc >= n) {
+							end[top] = i;
+				// System.out.println(top + " " + end[top]);
+							vec.removeElementAt(0);	
+						} else break;
+					}
+				}
 			}
-			
+
 			
 			int result = 0;
-			for (int start = 0; start <= name.length()-n; start++) {
-				for (int end = start+n; end <= name.length(); end++) {
-					//System.out.println(start + " " + end);
-					for (int i = start+n-1; i < end; i++) {
-						if (count[i] >= n) {
-							result++;
-							break;
-						}
-					}
+			for (int i = 0; i < name.length(); i++) {
+				if (end[i] >= 0) {
+					result += name.length() - end[i];
 				}
 			}
 			System.out.println("Case #" + (c++) + ": " + result);
