9964fbdd0f0e7dbafdc30d53b2f249bbe3e7917f
Xianming Zhong <chinesedfan@qq.com>
2019-05-26 18:57:19 +0800

    break if no better, fail sample

--- 2019/kickstart/roundc/b.js
+++ 2019/kickstart/roundc/b.js
@@ -28,16 +28,24 @@ function solve(r, c, k, grid) {
     for (var bc = 0; bc < grid[0].length; bc++) {
         var m = Array(grid.length);
         for (var ec = bc; ec < grid[0].length; ec++) {
+            var noBetter = false;
             for (var i = 0; i < grid.length; i++) {
                 m[i] = m[i] || {min: Infinity, max: -Infinity};
+                var old = m[i].invalid;
                 m[i].min = Math.min(grid[i][ec], m[i].min);
                 m[i].max = Math.max(grid[i][ec], m[i].max);
+                m[i].invalid = m[i].max - m[i].min > k;
+                if (!old && m[i].invalid) {
+                    noBetter = true;
+                    break;
+                }
             }
+            if (noBetter) break;
 
             var mc = 0;
             var c = 0; // how many continues rows
             for (var i = 0; i < grid.length; i++) {
-                if (m[i].max - m[i].min > k) {
+                if (m[i].invalid) {
                     c = 0;
                 } else {
                     c++;
