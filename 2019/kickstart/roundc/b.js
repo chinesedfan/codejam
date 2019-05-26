d86e5c8ec5ddab265a37c4ebc3c8cb8b069b2a78
Xianming Zhong <chinesedfan@qq.com>
2019-05-26 18:45:35 +0800

    n^3, tle

--- 2019/kickstart/roundc/b.js
+++ 2019/kickstart/roundc/b.js
@@ -1,5 +1,8 @@
-var readline = require('readline');
+// var fs = require('fs');
+// var args = process.argv.slice(2);
+// var lines = fs.readFileSync(args[0]).toString().split('\n');
 
+var readline = require('readline');
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -21,70 +24,29 @@ rl.on('close', function() {
 });
 
 function solve(r, c, k, grid) {
-    // var cols = [];
-    // for (var i = 0; i < grid[0].length; i++) {
-    //     cols[i] = [];
-    //     for (var j = 0; j < grid.length; j++) {
-    //         cols[i].push(grid[j][i]);
-    //     }
-    // }
-
-    var rminRMQs = grid.map((row) => prepareRMQ(row, Math.min));
-    var rmaxRMQs = grid.map((row) => prepareRMQ(row, Math.max));
-    // var cminRMQs = grid.map((col) => prepareRMQ(col, Math.min));
-    // var cmaxRMQs = grid.map((col) => prepareRMQ(col, Math.max));
-
-    // var m = [];
-
     var size = -Infinity;
     for (var bc = 0; bc < grid[0].length; bc++) {
+        var m = Array(grid.length);
         for (var ec = bc; ec < grid[0].length; ec++) {
+            for (var i = 0; i < grid.length; i++) {
+                m[i] = m[i] || {min: Infinity, max: -Infinity};
+                m[i].min = Math.min(grid[i][ec], m[i].min);
+                m[i].max = Math.max(grid[i][ec], m[i].max);
+            }
+
             var mc = 0;
             var c = 0; // how many continues rows
             for (var i = 0; i < grid.length; i++) {
-                if (find(rmaxRMQs[i], bc, ec, Math.max) - find(rminRMQs[i], bc, ec, Math.min) > k) {
+                if (m[i].max - m[i].min > k) {
                     c = 0;
                 } else {
                     c++;
                     mc = Math.max(mc, c);
                 }
             }
-            // console.log(bc, ec, mc)
             var nsize = mc * (ec - bc + 1);
             if (nsize > size) size = nsize;
         }
     }
     return size;
-}
-
-function prepareRMQ(ds, fn) {
-    var ret = [];
-
-    var p = 0;
-    var step = 1;
-    while (step <= ds.length) {
-        ret.push([]);
-
-        for (var i = 0; i < ds.length; i++) {
-            if (p == 0) {
-                ret[p][i] = ds[i];
-            } else {
-                ret[p][i] = fn(ret[p - 1][i], i + step / 2 < ds.length ? ret[p - 1][i + step / 2] : -Infinity);
-            }
-        }
-
-        p++;
-        step <<= 1;
-    }
-
-    return ret;
-}
-function find(m, l, r, fn) {
-    var p = 0;
-    var step = 1;
-    while (step * 2 <= r - l) {
-        p++;
-        step <<= 1;
-    }
-    return fn(m[p][l], m[p][r - step + 1]);
 }
