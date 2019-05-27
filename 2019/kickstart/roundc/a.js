d6437ff3ca5a778baf70a96e3569d83da1bb57d2
Xianming Zhong <chinesedfan@qq.com>
2019-05-27 21:20:30 +0800

    intervals, wa

--- 2019/kickstart/roundc/a.js
+++ 2019/kickstart/roundc/a.js
@@ -18,40 +18,94 @@ rl.on('close', function() {
 });
 
 function solve(r, c, sr, sc, instrs) {
-    var visited = {};
+    var rm = {}; // r -> [{beg, end}]
+    rm[sr] = [{beg: sc, end: sc}];
+    var cm = {};
+    cm[sc] = [{beg: sr, end: sr}];
+
     var p = {
         r: sr,
         c: sc
     };
-    visited[key(p)] = 1;
     for (var i = 0; i < instrs.length; i++) {
         var ch = instrs[i];
         switch (ch) {
         case 'N':
-            p.r--;
-            while (visited[key(p)]) p.r--;
-            visited[key(p)] = 1;
+            p.r = next(cm, p.c, p.r, -1);
+            next(rm, p.r, p.c, 0);
             break;
         case 'E':
-            p.c++;
-            while (visited[key(p)]) p.c++;
-            visited[key(p)] = 1;
+            p.c = next(rm, p.r, p.c, 1);
+            next(cm, p.c, p.r, 0);
             break;
         case 'W':
-            p.c--;
-            while (visited[key(p)]) p.c--;
-            visited[key(p)] = 1;
+            p.c = next(rm, p.r, p.c, -1);
+            next(cm, p.c, p.r, 0);
             break;
         case 'S':
-            p.r++;
-            while (visited[key(p)]) p.r++;
-            visited[key(p)] = 1;
+            p.r = next(cm, p.c, p.r, 1);
+            next(rm, p.r, p.c, 0);
             break;
         }
+        // console.log('rm', rm);
+        // console.log('cm', cm);
+        // console.log('p', p);
     }
 
     return p.r + ' ' + p.c;
 }
-function key(p) {
-    return p.r + '#' + p.c;
+function next(m, k, v, offset) {
+    v = v + offset;
+
+    var mk = m[k];
+    if (!mk) {
+        m[k] = [{beg: v, end: v}];
+        return v;
+    }
+
+    var ib = -1;
+    var ie = -1;
+    for (var i = 0; i < mk.length; i++) {
+        var o = mk[i];
+        if (v >= o.beg) ib = i;
+        if (v > o.end) ie = i;
+    }
+
+    // if inside, convert to gap
+    if (ib != ie) {
+        if (offset > 0) {
+            v = mk[ib].end + 1;
+            ie = ib;
+        } else if (offset < 0) {
+            v = mk[ib].beg - 1;
+            ib = ie;
+        } else {
+            return v;
+        }
+    }
+
+    if (ib < 0) {
+        if (v == mk[ib + 1].beg - 1) {
+            return --mk[ib + 1].beg;
+        } else {
+            mk.unshift({beg: v, end: v});
+        }
+    } else if (ib == mk.length - 1) {
+        if (v == mk[ib].end + 1) {
+            return ++mk[ib].end;
+        } else {
+            mk.push({beg: v, end: v});
+        }
+    } else {
+        if (mk[ib].end + 2 == mk[ib + 1].beg) {
+            mk.splice(ib, 1);
+        } else if (v == mk[ib].beg - 1) {
+            return --mk[ib].beg;
+        } else if (v == mk[ib + 1].end + 1) {
+            return ++mk[ib + 1].end;
+        } else {
+            mk.unshift({beg: v, end: v});
+        }
+    }
+    return v;
 }
