71431479ae8e7d24e2b8ce95f91ad80e93f30ba6
Xianming Zhong <chinesedfan@qq.com>
2019-10-27 23:08:40 +0800

    try as answer, wa

--- 2019/kickstart/roundg/c.js
+++ 2019/kickstart/roundg/c.js
@@ -20,27 +20,63 @@ rl.on('close', function() {
 });
 
 function solve(n, h, aps, bps) {
-    var max = Math.pow(2, n);
-    var c = 0;
-    for (var i = 0; i < max; i++) {
-        for (var j = 0; j < max; j++) {
-            var valid = true;
-            for (var k = 0; k < n; k++) {
-                if (!isHappy(i, k) && !isHappy(j, k)) {
-                    valid = false;
-                    break;
-                }
-            }
-            if (!valid) continue;
+    var mid = Math.floor(n / 2);
+    var left = getAllPairs(aps.slice(0, mid), bps.slice(0, mid));
+    var right = getAllPairs(aps.slice(mid), bps.slice(mid));
 
-            var ap = aps.reduce((s, p, x) => s + (isHappy(i, x) ? p : 0), 0);
-            var bp = bps.reduce((s, p, x) => s + (isHappy(j, x) ? p : 0), 0);
-            if (ap >= h && bp >= h) c++;
-        } 
+    var c = 0;
+    for (var a1 in left) {
+        for (var a2 in right) {
+            if (+a1 + +a2 < h) continue;
+
+            for (var i = 0; i < left[a1].length; i++) {
+                var b1 = left[a1][i];
+                var idx = binarySearch(0, right[a2].length - 1, (x) => b1 + right[a2][x] >= h);
+                c += idx + 1;
+            }
+        }
     }
+
     return c;
 }
 
-function isHappy(idx, i) {
-    return idx & (1 << i); 
+function getAllPairs(aps, bps) {
+    var n = aps.length;
+    var max = Math.pow(3, n);
+
+    var hp = {}; // ah -> [bh], where bh is sorted dec
+    for (var i = 0; i < max; i++) {
+        var a = 0;
+        var b = 0;
+        var temp = i;
+        for (var j = 0; j < n; j++) {
+            var c = temp % 3;
+            if (c == 0) {
+                a += aps[j];
+                b += bps[j];
+            } else if (c == 1) {
+                a += aps[j];
+            } else {
+                b += bps[j];
+            }
+            temp /= 3;
+        }
+        hp[a] = hp[a] || [];
+        var idx = binarySearch(0, hp[a].length - 1, (x) => hp[a][x] > b);
+        hp[a].splice(idx + 1, 0, b);
+    }
+
+    return hp;
+}
+
+function binarySearch(l, r, fn) { // for any [l, x], fn returns true
+    while (l <= r) {
+        var middle = Math.floor((l + r) / 2);
+        if (fn(middle)) {
+            l = middle + 1;
+        } else {
+            r = middle - 1;
+        }
+    }
+    return r;
 }
