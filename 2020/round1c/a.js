315de22c797df8e80ca54838a6da823c92a47af9
Xianming Zhong <chinesedfan@qq.com>
2020-05-02 17:14:40 +0800

    except a last

---
+++ 2020/round1c/a.js
@@ -0,0 +1,35 @@
+var readline = require('readline');
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+var lines = [];
+rl.on('line', function (input) {
+    lines.push(input);
+});
+rl.on('close', function () {
+    var t = parseInt(lines[0]);
+    var l = 1;
+    for (var i = 0; i < t; i++) {
+        var tokens = lines[l++].split(' ');
+        console.log('Case #%d: %s', i + 1, solve(+tokens[0], +tokens[1], tokens[2]));
+    }
+});
+
+function solve(x, y, ps) {
+    if (x === 0 && y === 0) return 0;
+
+    for (var i = 0; i < ps.length; i++) {
+        var ch = ps[i];
+        if (ch === 'S') {
+            y--;
+        } else if (ch === 'N') {
+            y++;
+        }
+
+        if (x + Math.abs(y) <= i + 1) return i + 1;
+    }
+    return 'IMPOSSIBLE';
+}
+
