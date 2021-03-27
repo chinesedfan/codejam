efda227646294b13952a5005e2d55b414c1ac5a8
Xianming Zhong <chinesedfan@qq.com>
2021-03-27 09:38:38 +0800

    select the max indexes sum of correct questions, wa for set 2

--- 2021/qualification/e.js
+++ 2021/qualification/e.js
@@ -34,18 +34,42 @@ rl.on('close', function() {
 // })();
 
 function solve(lines) {
-    // correct rate for each player
-    const cp = lines.map((line, idx) => {
+    // correct rate for each question
+    const qp = []
+    for (let i = 0; i < lines[0].length; i++) {
         let correct = 0
-        for (let i = 0; i < line.length; i++) {
+        lines.forEach(line => {
             if (line[i] === '1') correct++
+        })
+        qp[i] = {
+            q: i,
+            p: correct / lines[0].length
+        }
+    }
+    // the easiest question has the highest p
+    qp.sort((a, b) => b.p - a.p)
+
+    const map = {}
+    qp.forEach(({ q }, i) => {
+        map[q] = i
+    })
+
+    // index sum of right questions for each player
+    const cp = lines.map((line, idx) => {
+        let sum = 0
+        for (let i = 0; i < line.length; i++) {
+            if (line[i] === '1') {
+                sum += map[i]
+            }
         }
         return {
             idx: idx + 1,
-            p: correct / line.length
+            sum
         }
     })
-    return cp.sort((a, b) => b.p - a.p)[0].idx
+    return cp.sort((a, b) => b.sum - a.sum)[0].idx
+        // .slice(0, 10)
+        // .map(o => JSON.stringify(o))
 }
 
 function expected(diff) {
