bca3636571568def0f84f571e97de4c4c9c0806b
Xianming Zhong <chinesedfan@qq.com>
2021-04-10 10:17:15 +0800

    b without set 3

---
+++ 2021/round1a/b.js
@@ -0,0 +1,62 @@
+var readline = require('readline');
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+var lines = [];
+rl.on('line', function(input) {
+    lines.push(input);
+});
+rl.on('close', function() {
+    var t = parseInt(lines[0]);
+    var l = 1;
+    for (var i = 0; i < t; i++) {
+        var n = +lines[l++];
+        var ps = lines.slice(l, l + n).map(str => str.split(' ').map(Number))
+        console.log('Case #%d: %s', i + 1, solve(n, ps));
+        l += n
+    }
+});
+
+function solve(n, ps) {
+    const sum = ps.reduce((s, [p, c]) => s + p * c, 0)
+    for (let i = sum; i > 2; i--) {
+        if (able(ps, sum, i)) return i
+    }
+    return 0
+}
+
+function getPrimes() {
+    const str = '2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 101 103 107 109 113 127 131 137 139 149 151 157 163 167 173 179 181 191 193 197 199 211 223 227 229 233 239 241 251 257 263 269 271 277 281 283 293 307 311 313 317 331 337 347 349 353 359 367 373 379 383 389 397 401 409 419 421 431 433 439 443 449 457 461 463 467 479 487 491 499'
+    return str.split(' ').map(Number)
+}
+function able(ps, sum, target) {
+    const primes = getPrimes()
+    const ds = {}
+    let x = target
+    for (let i = 0; i < primes.length; i++) {
+        const p = primes[i]
+        if (p > x) break
+
+        while ((x % p) === 0) {
+            x /= p
+            ds[p] = (ds[p] || 0) + 1
+        }
+    }
+    if (x !== 1) return false
+
+    let rest = 0
+    for (let i = 0; i < ps.length; i++) {
+        const [p, c] = ps[i]
+        if (ds[p]) {
+            if (ds[p] > c) return false
+            rest += p * (c - ds[p])
+            delete ds[p]
+        } else {
+            rest += p * c
+        }
+    }
+    if (Object.keys(ds).length !== 0) return false
+    return rest === target
+}
