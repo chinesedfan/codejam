2a807ef2c06f3bd0cf68c2fe65280d748af1b3ae
Xianming Zhong <chinesedfan@qq.com>
2022-05-24 11:28:37 +0800

    solve b

--- 2022/kickstart/roundc/b.js
+++ 2022/kickstart/roundc/b.js
@@ -27,42 +27,19 @@ function solve(n, x, y) {
 
     const vx = sum / (x + y) * x
     const vy = sum / (x + y) * y
-// console.log(vx, vy)
-    const dp = []
-    for (let i = 1; i <= n; i++) {
-        const x = i
-        dp[i] = []
-        for (let j = 0; j <= vx; j++) {
-            if (i === 1) {
-                dp[i][j] = j - x >= 0 ? x : 0
-            } else if (j - x >= 0) {
-                dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - x] + x)
-            } else {
-                dp[i][j] = dp[i - 1][j]
-            }
-        }
-    }
-    if (dp[n][vx] !== vx) return 'IMPOSSIBLE'
-    //
-    const ans = Array(n + 1).fill(0)
-    let now = vx
-    for (let i = n; i >= 1; i--) {
-        const prev = i > 1 ? dp[i - 1][now] : 0
-        if (dp[i][now] !== prev) {
-            ans[i] = 1
-            now -= i
-        }
-    }
 
-// console.log(ans)
-    const sa = []
-    const sb = []
-    for (let i = 1; i <= n; i++) {
-        if (ans[i]) {
-            sa.push(i)
-        } else {
-            sb.push(i)
+    let ans = []
+    let i = 1
+    let now = 0
+    while (i <= n && now + i <= vx) {
+        now += i
+        ans.push(i++)
+    }
+    const r = vx - now
+    if (r) {
+        for (let i = 0; i < r; i++) {
+            ans[ans.length - 1 - i]++
         }
     }
-    return 'POSSIBLE\n' + sa.length + '\n' + sa.join(' ')
+    return 'POSSIBLE\n' + ans.length + '\n' + ans.join(' ')
 }
